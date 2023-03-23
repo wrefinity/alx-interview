04e39bd7020dd6e5247265501cf4169b8d3dad72
root <wrefordmessi@gmail.com>
2023-03-23 13:09:55 -0700

    pascal traingle updates

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,19 +1,30 @@
 #!/usr/bin/python3
-''' Pascal Traingle '''
+""" pascal triangle
+"""
 
 
 def pascal_triangle(n):
-
+    """ returns pascal triangle
+    """
     if n <= 0:
         return []
-    pas = [[] for c in range(n)]
-    for row in range(n):
-        for col in range(row + 1):
-            if (col < row):
-                if col == 0:
-                    pas[row].append(1)
-                else:
-                    pas[row].append(pas[row-1][col] + pas[row-1][col-1])
-            elif(col == row):
-                pas[row].append(1)
+
+    pas = []
+
+    for i in range(n):
+        r_List = [1]
+        if i == 0:
+            pas.append(r_List)
+            continue
+
+        k = i - 1
+        for j in range(len(pas[k])):
+            if j + 1 == len(pas[k]):
+                r_List.append(1)
+                break
+            # adding the result of previous row and col
+            res = pas[k][j] + pas[k][j + 1]
+            r_List.append(res)
+        pas.append(r_List)
+
     return pas
