eea0759c117e5adad01e69dc8fe2c6176f842bae
root <wrefordmessi@gmail.com>
2023-04-13 16:12:02 -0700

    stats checker

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""Write a script that reads stdin line by line
+and prints stats to stdout
+"""
+
+
+import sys
+
+# status codes dictionary
+status_codes_dict = {'200': 0, '301': 0, '400': 0, '401': 0, '403': 0,
+                     '404': 0, '405': 0, '500': 0}
+
+total_size = 0
+count = 0  # line counter
+
+try:
+    for line in sys.stdin:
+        line_list = line.split(" ")
+
+        if len(line_list) > 4:
+            status_code = line_list[-2]
+            file_size = int(line_list[-1])
+
+            # check if the status code passed exists then
+            # increment its count
+            if status_code in status_codes_dict.keys():
+                status_codes_dict[status_code] += 1
+
+            # increment total size
+            total_size += file_size
+
+            # update line counter
+            count += 1
+
+        if count == 10:
+            count = 0  # reset counter
+            print('File size: {}'.format(total_size))
+
+            # print out status code counts
+            for key, value in sorted(status_codes_dict.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except Exception as err:
+    pass
+
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(status_codes_dict.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
