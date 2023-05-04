62955dedffaaa853cd9cf14c83ad592cc16b6ffa
root <wrefordmessi@gmail.com>
2023-05-04 13:27:39 -0700

    movie bug

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -4,24 +4,53 @@
 */
 
 const request = require('request');
-const starWarsAPI = 'https://swapi-api.alx-tools.com/api/';
-const endPoint = 'films/';
-const movieId = process.argv[2].toString();
 
-request(starWarsAPI + endPoint + movieId, function (error, _, body) {
-  if (error) console.error(error);
-  const objects = JSON.parse(body);
-  const chars = objects.characters;
-  PrintResult(chars);
-});
+const movieId = process.argv[2];
+const filmEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
+let people = [];
+const names = [];
 
-/* iteratively request for each character */
-function PrintResult (chars, inc = 0) {
-  request(chars[counter], function (err, _, body) {
-    if (err) console.error(err);
-    console.log(JSON.parse(body).name);
-    if (++inc < chars.length) {
-      PrintResult(chars, inc++);
+const queryCharacters = async () => {
+  await new Promise(resolve => request(filmEndPoint, (err, res, body) => {
+    if (err || res.statusCode !== 200) {
+      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+    } else {
+      const jsonBdy = JSON.parse(body);
+      people = jsonBdy.characters;
+      resolve();
     }
-  });
-}
+  }));
+};
+
+const queryNames = async () => {
+  if (people.length > 0) {
+    for (const p of people) {
+      await new Promise(resolve => request(p, (err, res, body) => {
+        if (err || res.statusCode !== 200) {
+          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+        } else {
+          const jsonBdy = JSON.parse(body);
+          names.push(jsonBdy.name);
+          resolve();
+        }
+      }));
+    }
+  } else {
+    console.error('Error: Got no Characters for some reason');
+  }
+};
+
+const getMovieCast = async () => {
+  await queryCharacters();
+  await queryNames();
+
+  for (const nm of names) {
+    if (nm === names[names.length - 1]) {
+      process.stdout.write(nm);
+    } else {
+      process.stdout.write(nm + '\n');
+    }
+  }
+};
+
+getMovieCast();
