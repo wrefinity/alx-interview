83433558513351344774f49350e4d5e8d4bd49c7
root <wrefordmessi@gmail.com>
2023-05-04 16:37:10 -0700

    character update

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,29 +3,51 @@
 const request = require('request');
 
 const movieId = process.argv[2];
-const moveURL = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+const API_URL = 'https://swapi-api.alx-tools.com/api/films/' + movieId;
+let actors = [];
+const names = [];
 
-function sendRequest (characterList, index) {
-  if (characterList.length === index) {
-    return;
-  }
-
-  request(characterList[index], (error, response, body) => {
-    if (error) {
-      console.log(error);
+const queryCharacters = async () => {
+  await new Promise(resolve => request(API_URL, (err, res, body) => {
+    if (err || res.statusCode !== 200) {
+      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
     } else {
-      console.log(JSON.parse(body).name);
-      sendRequest(characterList, index + 1);
+      const jsonBody = JSON.parse(body);
+      actors = jsonBody.characters;
+      resolve();
     }
-  });
-}
+  }));
+};
 
-request(movieURL, (error, _, body) => {
-  if (err) {
-    console.log(err);
+const queryNames = async () => {
+  if (actors.length > 0) {
+    for (const ac of actors) {
+      await new Promise(resolve => request(ac, (err, res, body) => {
+        if (err || res.statusCode !== 200) {
+          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
+        } else {
+          const jsonBody = JSON.parse(body);
+          names.push(jsonBody.name);
+          resolve();
+        }
+      }));
+    }
   } else {
-    const characterx = JSON.parse(body).characters;
-
-    sendRequest(characterx, 0);
+    console.error('Error: No Charcter Found');
   }
-});
+};
+
+const getCharNames = async () => {
+  await queryCharacters();
+  await queryNames();
+
+  for (const n of names) {
+    if (n === names[names.length - 1]) {
+      process.stdout.write(n);
+    } else {
+      process.stdout.write(n + '\n');
+    }
+  }
+};
+
+getCharNames();
