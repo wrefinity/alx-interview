95a71d3666a980803f69fa517415a0d11412b4b0
root <wrefordmessi@gmail.com>
2023-05-04 15:17:13 -0700

    package addon

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,56 +1,25 @@
 #!/usr/bin/node
-/* 
- * Get all Casts of Star Wars movie
-*/
-
 const request = require('request');
+const FILM_URL = 'https://swapi-api.hbtn.io/api';
 
-const movieId = process.argv[2];
-const filmEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
-let people = [];
-const names = [];
-
-const queryCharacters = async () => {
-  await new Promise(resolve => request(filmEndPoint, (err, res, body) => {
-    if (err || res.statusCode !== 200) {
-      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
-    } else {
-      const jsonBdy = JSON.parse(body);
-      people = jsonBdy.characters;
-      resolve();
+if (process.argv.length > 2) {
+  request(`${FLIM_URL}/films/${process.argv[2]}/`, (err, _, body) => {
+    if (err) {
+      console.log(err);
     }
-  }));
-};
-
-const queryNames = async () => {
-  if (people.length > 0) {
-    for (const p of people) {
-      await new Promise(resolve => request(p, (err, res, body) => {
-        if (err || res.statusCode !== 200) {
-          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
-        } else {
-          const jsonBdy = JSON.parse(body);
-          names.push(jsonBdy.name);
-          resolve();
-        }
+    const charactersURL = JSON.parse(body).characters;
+    const charactersName = charactersURL.map(
+      url => new Promise((resolve, reject) => {
+        request(url, (promiseErr, __, charactersReqBody) => {
+          if (promiseErr) {
+            reject(promiseErr);
+          }
+          resolve(JSON.parse(charactersReqBody).name);
+        });
       }));
-    }
-  } else {
-    console.error('Error: Got no Characters for some reason');
-  }
-};
 
-const getMovieCast = async () => {
-  await queryCharacters();
-  await queryNames();
-
-  for (const nm of names) {
-    if (nm === names[names.length - 1]) {
-      process.stdout.write(nm);
-    } else {
-      process.stdout.write(nm + '\n');
-    }
-  }
-};
-
-getMovieCast();
+    Promise.all(charactersName)
+      .then(names => console.log(names.join('\n')))
+      .catch(allErr => console.log(allErr));
+  });
+}
